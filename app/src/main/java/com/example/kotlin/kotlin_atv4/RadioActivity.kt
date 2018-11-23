c90844e0401a45c012fbbf9514b7b88452e5d3ad
cesarwisl <cesarwisl@Administrators-MacBook-Pro.local>
2018-11-23 15:34:10 -0300

    init project

---
+++ app/src/main/java/com/example/kotlin/kotlin_atv4/RadioActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kotlin.kotlin_atv4
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RadioActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_radio)
+    }
+}
