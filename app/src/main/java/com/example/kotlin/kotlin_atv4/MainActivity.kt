04f7356fceef82a2a16c98c6f011ac43f776d5d0
MattLack <a.mateuslol@gmail.com>
2018-11-23 23:55:59 -0300

    implementando

--- app/src/main/java/com/example/kotlin/kotlin_atv4/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/kotlin_atv4/MainActivity.kt
@@ -1,12 +1,59 @@
 package com.example.kotlin.kotlin_atv4
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_radio.*
 
 class MainActivity : AppCompatActivity() {
+
+    var result: Int? = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //val i = Intent(this, RadioActivity::class.java)
+
+        //refreshText(i.getIntExtra("result",0 ))
+
+//        if ((i.getIntExtra("result") != null)
+//                && (savedInstanceState.getInt("result") != null)){
+//            refreshText(savedInstanceState.get("result") as Int)
+//        }
+
+
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == result && resultCode == 1){
+            btnSelect.text = getString(R.string.rdbtnBcc)
+        }
+        if(requestCode == result && resultCode == 2){
+            btnSelect.text = getString(R.string.rdbtnBsi)
+        }
+        if(requestCode == result && resultCode == 3){
+            btnSelect.text = getString(R.string.rdbtnLc)
+        }
+    }
+
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        result = savedInstanceState.getInt("result")
+    }
+
+
+    fun gotoRadioActivity(view : View){
+        val intent = Intent(applicationContext,RadioActivity::class.java)
+        // intent.putExtra("input",editText.text.toString())
+        startActivity(intent)
+    }
+
+
 }
+
--- app/src/main/java/com/example/kotlin/kotlin_atv4/RadioActivity.kt
+++ app/src/main/java/com/example/kotlin/kotlin_atv4/RadioActivity.kt
@@ -1,12 +1,49 @@
 package com.example.kotlin.kotlin_atv4
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import kotlinx.android.synthetic.main.activity_radio.*
+import kotlinx.android.synthetic.main.activity_radio.view.*
 
 class RadioActivity : AppCompatActivity() {
+
+    var result: Int? = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_radio)
+
+        rdgroup.rdbtnBcc.isChecked = true
+
+
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("result", result!!)
+    }
+
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        result = savedInstanceState.getInt("result")
+    }
+
+    fun gotoMainActivity(view: View) {
+        val intent = Intent(applicationContext, MainActivity::class.java)
+           // val radio: RadioButton = view.findViewById(rdgroup.checkedRadioButtonId)
+            var id: Int = rdgroup.checkedRadioButtonId
+            //val radio: RadioButton = findViewById(id)
+            intent.putExtra("result", id)
+            startActivity(intent)
+    }
+
+    override fun startActivityForResult(intent: Intent?, requestCode: Int) {
+        super.startActivityForResult(intent, requestCode)
+
+    }
+
 }
